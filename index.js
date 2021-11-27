3ebda60d25f23ad334960c2f421510204d4f23c7
RainAndrewAndre <77835007+JotaroKujo0525@users.noreply.github.com>
2021-11-27 04:27:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const topic = require("./scr/topic.json")
+const wyr = require("./scr/wyr.json")
+const path = require("path")
+
+app.use(express.static("public"));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get("/topic", (req, res) => {
+  var item = topic[Math.floor(Math.random() * topic.length)]
+  res.json(item)
+});
+
+app.get("/wyr", (req, res) => {
+  var itemwyr = wyr[Math.floor(Math.random() * wyr.length)]
+  res.json(itemwyr)
+})
+
+
+const listener = app.listen(3000, () => {
+  console.log('server started');
+});
